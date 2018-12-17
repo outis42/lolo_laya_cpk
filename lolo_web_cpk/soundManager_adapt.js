6af67dccdec52d81580dedc4e635e7c44a136222
Outis <huangxikai42@gmail.com>
2018-12-17 11:40:22 +0800

    需要适配的音频类

---
+++ lolo_web_cpk/soundManager_adapt.js
@@ -0,0 +1,305 @@
+/**
+*<code>SoundManager</code> 是一个声音管理类。提供了对背景音乐、音效的播放控制方法。
+*引擎默认有两套声音方案：WebAudio和H5Audio
+*播放音效，优先使用WebAudio播放声音，如果WebAudio不可用，则用H5Audio播放，H5Audio在部分机器上有兼容问题（比如不能混音，播放有延迟等）。
+*播放背景音乐，则使用H5Audio播放（使用WebAudio会增加特别大的内存，并且要等加载完毕后才能播放，有延迟）
+*建议背景音乐用mp3类型，音效用wav或者mp3类型（如果打包为app，音效只能用wav格式）。
+*详细教程及声音格式请参考：http://ldc.layabox.com/doc/?nav=ch-as-1-7-0
+*/
+//class laya.media.SoundManager
+var SoundManager=(function(){
+	function SoundManager(){}
+	__class(SoundManager,'laya.media.SoundManager');
+	__getset(1,SoundManager,'useAudioMusic',function(){
+		return SoundManager._useAudioMusic;
+		},function(value){
+		SoundManager._useAudioMusic=value;
+		if (value){
+			SoundManager._musicClass=AudioSound;
+			}else{
+			SoundManager._musicClass=null;
+		}
+	});
+
+	/**
+	*失去焦点后是否自动停止背景音乐。
+	*@param v Boolean 失去焦点后是否自动停止背景音乐。
+	*
+	*/
+	/**
+	*失去焦点后是否自动停止背景音乐。
+	*/
+	__getset(1,SoundManager,'autoStopMusic',function(){
+		return SoundManager._autoStopMusic;
+		},function(v){
+		Laya.stage.off(/*laya.events.Event.BLUR*/"blur",null,SoundManager._stageOnBlur);
+		Laya.stage.off(/*laya.events.Event.FOCUS*/"focus",null,SoundManager._stageOnFocus);
+		Laya.stage.off(/*laya.events.Event.VISIBILITY_CHANGE*/"visibilitychange",null,SoundManager._visibilityChange);
+		SoundManager._autoStopMusic=v;
+		if (v){
+			Laya.stage.on(/*laya.events.Event.BLUR*/"blur",null,SoundManager._stageOnBlur);
+			Laya.stage.on(/*laya.events.Event.FOCUS*/"focus",null,SoundManager._stageOnFocus);
+			Laya.stage.on(/*laya.events.Event.VISIBILITY_CHANGE*/"visibilitychange",null,SoundManager._visibilityChange);
+		}
+	});
+
+	/**
+	*背景音乐和所有音效是否静音。
+	*/
+	__getset(1,SoundManager,'muted',function(){
+		return SoundManager._muted;
+		},function(value){
+		if (value==SoundManager._muted)return;
+		if (value){
+			SoundManager.stopAllSound();
+		}
+		SoundManager.musicMuted=value;
+		SoundManager._muted=value;
+	});
+
+	/**
+	*背景音乐（不包括音效）是否静音。
+	*/
+	__getset(1,SoundManager,'musicMuted',function(){
+		return SoundManager._musicMuted;
+		},function(value){
+		if (value==SoundManager._musicMuted)return;
+		if (value){
+			if (SoundManager._tMusic){
+				if (SoundManager._musicChannel&&!SoundManager._musicChannel.isStopped){
+					SoundManager._musicChannel.pause();
+					}else{
+					SoundManager._musicChannel=null;
+				}
+				}else{
+				SoundManager._musicChannel=null;
+			}
+			SoundManager._musicMuted=value;
+			}else {
+			SoundManager._musicMuted=value;
+			if (SoundManager._tMusic){
+				if (SoundManager._musicChannel){
+					SoundManager._musicChannel.resume();
+				}
+			}
+		}
+	});
+
+	/**
+	*所有音效（不包括背景音乐）是否静音。
+	*/
+	__getset(1,SoundManager,'soundMuted',function(){
+		return SoundManager._soundMuted;
+		},function(value){
+		SoundManager._soundMuted=value;
+	});
+
+	SoundManager.addChannel=function(channel){
+		if (SoundManager._channels.indexOf(channel)>=0)return;
+		SoundManager._channels.push(channel);
+	}
+
+	SoundManager.removeChannel=function(channel){
+		var i=0;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			if (SoundManager._channels[i]==channel){
+				SoundManager._channels.splice(i,1);
+			}
+		}
+	}
+
+	SoundManager.disposeSoundIfNotUsed=function(url){
+		var i=0;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			if (SoundManager._channels[i].url==url){
+				return;
+			}
+		}
+		SoundManager.destroySound(url);
+	}
+
+	SoundManager._visibilityChange=function(){
+		if (Laya.stage.isVisibility){
+			SoundManager._stageOnFocus();
+			}else {
+			SoundManager._stageOnBlur();
+		}
+	}
+
+	SoundManager._stageOnBlur=function(){
+		SoundManager._isActive=false;
+		if (SoundManager._musicChannel){
+			if (!SoundManager._musicChannel.isStopped){
+				SoundManager._blurPaused=true;
+				SoundManager._musicChannel.pause();
+			}
+		}
+		SoundManager.stopAllSound();
+		Laya.stage.once(/*laya.events.Event.MOUSE_DOWN*/"mousedown",null,SoundManager._stageOnFocus);
+	}
+
+	SoundManager._recoverWebAudio=function(){
+		if(WebAudioSound.ctx&&WebAudioSound.ctx.state!="running"&&WebAudioSound.ctx.resume)
+			WebAudioSound.ctx.resume();
+	}
+
+	SoundManager._stageOnFocus=function(){
+		SoundManager._isActive=true;
+		SoundManager._recoverWebAudio();
+		Laya.stage.off(/*laya.events.Event.MOUSE_DOWN*/"mousedown",null,SoundManager._stageOnFocus);
+		if (SoundManager._blurPaused){
+			if (SoundManager._musicChannel && SoundManager._musicChannel.isStopped){
+				SoundManager._blurPaused=false;
+				SoundManager._musicChannel.resume();
+			}
+		}
+	}
+
+	SoundManager.playSound=function(url,loops,complete,soundClass,startTime){
+		(loops===void 0)&& (loops=1);
+		(startTime===void 0)&& (startTime=0);
+		if (!SoundManager._isActive || !url)return null;
+		if (SoundManager._muted)return null;
+		SoundManager._recoverWebAudio();
+		url=URL.formatURL(url);
+		if (url==SoundManager._tMusic){
+			if (SoundManager._musicMuted)return null;
+			}else {
+			if (Render.isConchApp){
+				var ext=Utils.getFileExtension(url);
+				if (ext !="wav" && ext !="ogg"){
+					alert("The sound only supports wav or ogg format,for optimal performance reason,please refer to the official website document.");
+					return null;
+				}
+			}
+			if (SoundManager._soundMuted)return null;
+		};
+		var tSound;
+		if (!Browser.onMiniGame){
+			tSound=Laya.loader.getRes(url);
+		}
+		if (!soundClass)soundClass=SoundManager._soundClass;
+		if (!tSound){
+			tSound=new soundClass();
+			tSound.load(url);
+			if (!Browser.onMiniGame){
+				Loader.cacheRes(url,tSound);
+			}
+		};
+		var channel;
+		channel=tSound.play(startTime,loops);
+		if (!channel)return null;
+		channel.url=url;
+		channel.volume=(url==SoundManager._tMusic)? SoundManager.musicVolume :SoundManager.soundVolume;
+		channel.completeHandler=complete;
+		return channel;
+	}
+
+	SoundManager.destroySound=function(url){
+		var tSound=Laya.loader.getRes(url);
+		if (tSound){
+			Loader.clearRes(url);
+			tSound.dispose();
+		}
+	}
+
+	SoundManager.playMusic=function(url,loops,complete,startTime){
+		(loops===void 0)&& (loops=0);
+		(startTime===void 0)&& (startTime=0);
+		url=URL.formatURL(url);
+		SoundManager._tMusic=url;
+		if (SoundManager._musicChannel)SoundManager._musicChannel.stop();
+		return SoundManager._musicChannel=SoundManager.playSound(url,loops,complete,SoundManager._musicClass,startTime);
+	}
+
+	SoundManager.stopSound=function(url){
+		url=URL.formatURL(url);
+		var i=0;
+		var channel;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			channel=SoundManager._channels[i];
+			if (channel.url==url){
+				channel.stop();
+			}
+		}
+	}
+
+	SoundManager.stopAll=function(){
+		SoundManager._tMusic=null;
+		var i=0;
+		var channel;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			channel=SoundManager._channels[i];
+			channel.stop();
+		}
+	}
+
+
+	SoundManager.stopAllSound=function(){
+		var i=0;
+		var channel;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			channel=SoundManager._channels[i];
+			if (channel.url !=SoundManager._tMusic){
+				channel.stop();
+			}
+		}
+	}
+
+	SoundManager.stopMusic=function(){
+		if (SoundManager._musicChannel)SoundManager._musicChannel.stop();
+		SoundManager._tMusic=null;
+	}
+
+	SoundManager.setSoundVolume=function(volume,url){
+		if (url){
+			url=URL.formatURL(url);
+			SoundManager._setVolume(url,volume);
+			}else {
+			SoundManager.soundVolume=volume;
+			var i=0;
+			var channel;
+			for (i=SoundManager._channels.length-1;i >=0;i--){
+				channel=SoundManager._channels[i];
+				if (channel.url !=SoundManager._tMusic){
+					channel.volume=volume;
+				}
+			}
+		}
+	}
+
+	SoundManager.setMusicVolume=function(volume){
+		SoundManager.musicVolume=volume;
+		SoundManager._setVolume(SoundManager._tMusic,volume);
+	}
+
+	SoundManager._setVolume=function(url,volume){
+		url=URL.formatURL(url);
+		var i=0;
+		var channel;
+		for (i=SoundManager._channels.length-1;i >=0;i--){
+			channel=SoundManager._channels[i];
+			if (channel.url==url){
+				channel.volume=volume;
+			}
+		}
+	}
+
+	SoundManager.musicVolume=1;
+	SoundManager.soundVolume=1;
+	SoundManager.playbackRate=1;
+	SoundManager._useAudioMusic=true;
+	SoundManager._muted=false;
+	SoundManager._soundMuted=false;
+	SoundManager._musicMuted=false;
+	SoundManager._tMusic=null;
+	SoundManager._musicChannel=null;
+	SoundManager._channels=[];
+	SoundManager._autoStopMusic=false;
+	SoundManager._blurPaused=false;
+	SoundManager._isActive=true;
+	SoundManager._soundClass=null;
+	SoundManager._musicClass=null;
+	SoundManager.autoReleaseSound=true;
+	return SoundManager;
+})()
